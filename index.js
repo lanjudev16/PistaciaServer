cb84a33440887b739e268fd1804c017d7cded476
lanjudev16 <lanjudev16@gmail.com>
2023-05-18 15:43:03 +0600

    toy db created and insert a data

--- index.js
+++ index.js
@@ -25,6 +25,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db('toyDb');
+    const toyCollection = database.collection('toys');
+
+
+    app.post('/addToy',async(req,res)=>{
+        const singleToyInfo=req.body
+        const result=await toyCollection.insertOne(singleToyInfo)
+        res.send(result)
+        console.log(singleToyInfo)
+    })
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -32,7 +53,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
